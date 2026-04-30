bed1c59f8e5a68552f589f0ff9c76dec239614aa
germaine <germaine.goetzelmann@kit.edu>
2026-04-30 17:47:06 +0200

    Removing exposure of svg files from index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,3 @@
-declare module "*.svg" {
-    const value: string;
-    export default value;
-}
-
 declare module "@toroll/tagin-icons" {
     export const icons: Record<string, string>;
     const defaultExport: Record<string, string>;
